f31c87a1c4be59ee9aaa12b8a542b6118a13ce01
gernas <4grns7@gmail.com>
2021-04-13 02:15:21 +0300

    21 Mart

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -23,7 +23,7 @@ namespace WebAPI.Controllers
 
         [HttpGet("getall")]
         public IActionResult GetAll()
-        {            
+        {
             var result = _productService.GetAll();
             if (result.Success)
             {
